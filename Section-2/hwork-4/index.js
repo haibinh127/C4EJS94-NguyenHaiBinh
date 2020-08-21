6a06c8f93a7812b2327587c83466cd5f77b0ca6f
haibinh127 <binhnguyen127@gmail.com>
2020-08-21 14:19:55 +0700

    Add index.html + index.js

--- Section-2/hwork-4/index.js
+++ Section-2/hwork-4/index.js
@@ -1,8 +1,9 @@
+console.log("Caculate the factorial of a number n=");
 n = prompt("Enter a number");
 y = 1;
 let i = 1;
 while (i <= n) {
-    y=y*i
+    y = y * i
     i++
 };
 alert(` The factorial of ${n} is ` + y);
